fece64da54851b685ee6552051797db27faf06d1
Prem B. Shah <techie.prembshah@gmail.com>
2023-09-19 19:58:32 +0530

    This adds the solution to problem 4

--- javaProblems/problem4/Solution.java
+++ javaProblems/problem4/Solution.java
@@ -1,5 +1,24 @@
 public class Solution {
     public int solve(String A) {
-        return -1;
+        //This variable keeps the count of occurrences of 'G' in the string.
+        int count = 0; 
+
+        //This variable keeps the count of occurrences of 'AG' in the string. 
+        int ans = 0; 
+
+        // We will traverse the string from the 'last index' to the '0th index', and as soon as we find the character 'A', we will add the 'count' variable to the variable 'ans'. Thus, at the end, the variable : 'ans' will give us the total number of substrings 'AG' present in the given string.
+
+        for(int i = A.length()-1; i >= 0; i--)
+        {
+            if(A.charAt(i) == 'G')
+            {
+                count++;
+            }
+            if(A.charAt(i) == 'A')
+            {
+                ans += count;
+            }
+        }
+        return ans;
     }
 }
