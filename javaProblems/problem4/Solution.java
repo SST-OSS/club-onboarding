749de0717b58234f2d3499410791bfafa0e979f4
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-09-19 20:52:57 +0530

    Solution to Java Problem 4
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- javaProblems/problem4/Solution.java
+++ javaProblems/problem4/Solution.java
@@ -1,5 +1,14 @@
 public class Solution {
     public int solve(String A) {
-        return -1;
+	    int ans=0;
+	    int gfreq=0;
+	    for(int i=A.length()-1;i>=0;i++){
+		    if(A.charAt(i)=='G'){
+		    gfreq++;
+		    }else if(A.charAt(i)=='A'){
+		    ans+=gfreq;
+		    }
+	    }
+	     return ans;
     }
 }
