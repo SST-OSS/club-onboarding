7d440e6b685b9180ce95234e0e01037843090d95
narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>
2023-09-19 13:33:00 +0530

    Soultion to problem 2 in java Problem
    Signed-off-by: narasimha-1511 <129654598+narasimha-1511@users.noreply.github.com>

--- javaProblems/problem2/Solution.java
+++ javaProblems/problem2/Solution.java
@@ -1,5 +1,10 @@
 public class Solution {
     public int[] solve(int[] A) {
-        return new int[] {};
+            for(int i=0;i<A.length/2;i++){
+		    int temp = A[i];
+		    A[i] = A[A.length-1-i];
+		    A[A.length-1-i]=temp;
+	    }    
+	    return A;
     }
 }
