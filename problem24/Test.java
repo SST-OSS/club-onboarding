583c14995efdce937a578689a27bfc4897234b93
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2023-09-20 18:53:21 +0530

    problem 24 test file updated

--- problem24/Test.java
+++ problem24/Test.java
@@ -1,38 +1,26 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SolutionTest {
-
-    @Test
-    public void testCommonChars() {
+    public static void main(String[] args) throws Exception {
+        Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
         Solution solution = new Solution();
-
+        
         // Test case 1: Example test case
         String[] input1 = {"bella", "label", "roller"};
         List<String> expected1 = Arrays.asList("e", "l", "l");
         List<String> result1 = solution.commonChars(input1);
-        assertEquals(expected1, result1);
-
-        // Test case 2: All strings have the same characters
-        String[] input2 = {"abcde", "abcde", "abcde"};
-        List<String> expected2 = Arrays.asList("a", "b", "c", "d", "e");
-        List<String> result2 = solution.commonChars(input2);
-        assertEquals(expected2, result2);
-
-        // Test case 3: No common characters
-        String[] input3 = {"abc", "def", "xyz"};
-        List<String> expected3 = Arrays.asList();
-        List<String> result3 = solution.commonChars(input3);
-        assertEquals(expected3, result3);
-
-        // Test case 4: Empty input
-        String[] input4 = {};
-        List<String> expected4 = Arrays.asList();
-        List<String> result4 = solution.commonChars(input4);
-        assertEquals(expected4, result4);
-
-        // Add more test cases as needed
+        if (!expected1.equals(result1)) {
+            logger.log(Level.SEVERE, "Test case 1 failed!");
+            System.exit(1);
+            throw new Exception("Test case 1 failed!");
+        }
+        
+        // Test case 2: Add more test cases here
+        
+        // All test cases passed
+        System.out.print("All test cases passed!");
     }
 }
