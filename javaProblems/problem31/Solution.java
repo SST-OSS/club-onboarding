0275da3d5384bfeb3063f3e33e796f8198b65d04
Varun Deep Saini <140642437+varundeepsaini@users.noreply.github.com>
2023-09-20 18:50:43 +0530

    Update Solution.java
    Updated Solution.java with the correct format

--- javaProblems/problem31/Solution.java
+++ javaProblems/problem31/Solution.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
 
-public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int num1 = sc.nextInt();
-    int num2 = sc.nextInt();
-    solve(num1, num2);
-  }
-
-  public static int gcd(int a, int b) {
+public class Solution{ 
+  public int gcd(int a, int b) {
     if (b == 0) {
       return a;
     }
     return gcd(b, a % b);
   }
 
-  public static void solve(int a, int b) {
-    int n = gcd(a, b);
-    System.out.println(n);
-    System.out.println((a * b) / n);
+  public int[]  solve(int num1, int num2) {
+    int arr[] = new int[2];
+    arr[0] = gcd(num1,num2);
+    arr[1] = (num1*num2)/arr[0];
+    return arr;
   }
 }
