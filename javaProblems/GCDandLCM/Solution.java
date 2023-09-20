83b87335153d8b535319446a2603ba2b7abb2a07
Varun Deep Saini <140642437+varundeepsaini@users.noreply.github.com>
2023-09-20 16:19:50 +0530

    Created Solution.java

---
+++ javaProblems/GCDandLCM/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+    solve(num1, num2);
+  }
+
+  public static int gcd(int a, int b) {
+    if (b == 0) {
+      return a;
+    }
+    return gcd(b, a % b);
+  }
+
+  public static void solve(int a, int b) {
+    int n = gcd(a, b);
+    System.out.println(n);
+    System.out.println((a * b) / n);
+  }
+}
