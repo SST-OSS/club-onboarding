0cc98d5096b21a25ea8d1627d939abab66ff5257
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2023-09-21 21:17:06 +0530

    updated test file, solution file

---
+++ javaProblems/problem24/Solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+class Solution {
+   public static ArrayList<String> solve(String[] A) {
+        int[] last = count(A[0]);
+        for (int i = 1; i < A.length; i++) {
+            last = intersection(last, count(A[i]));
+        }
+        ArrayList<String> arr = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            if (last[i] != 0) {
+                char a = 'a';
+                a += i;
+                String s = String.valueOf(a);
+                while (last[i] > 0) {
+                    arr.add(s);
+                    last[i]--;
+                }
+            }
+        }
+        return arr;
+    }
+    static int[] intersection(int[] a, int[] b) {
+        int[] t = new int[26];
+        for (int i = 0; i < 26; i++) {
+            t[i] = Math.min(a[i], b[i]);
+        }
+        return t;
+    }
+    static int[] count(String str) {
+        int[] t = new int[26];
+        for (char c : str.toCharArray()) t[c - 'a']++;
+        return t;
+    }
+}
