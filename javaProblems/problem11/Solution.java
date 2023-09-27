927e889cebe7cba41f66a1b1eec617325982ebb0
Srujan Dharma <140909375+srujan0404@users.noreply.github.com>
2023-09-28 01:11:44 +0530

    solved problem11 (#99)

---
+++ javaProblems/problem11/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        int haystackLength = haystack.length();
+        int needleLength = needle.length();
+        for (int i = 0; i <= haystackLength - needleLength; i++) {
+            int j = 0;
+            while (j < needleLength && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == needleLength) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
