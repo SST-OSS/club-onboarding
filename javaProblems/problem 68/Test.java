7aa20665cd1340b39cafb24a318b07a5fe26c735
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2023-09-30 00:12:40 +0530

    Update Test.java

--- javaProblems/problem 68/Test.java
+++ javaProblems/problem 68/Test.java
@@ -27,14 +27,4 @@ public class Test {
         return reversed;
     }
 }
-class Solution {
-    public int reverse(int n) {
-        int reversed = 0;
-        while (n != 0) {
-            int digit = n % 10;
-            reversed = reversed * 10 + digit;
-            n /= 10;
-        }
-        return reversed;
-    }
-}
+
