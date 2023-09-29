ce3025e261c037c9fb203abe8a2190afc7573c6b
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2023-09-29 23:54:26 +0530

    Create Test.java

---
+++ javaProblems/problem 68/Test.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ReverseNumberDigitsTest {
+    public void testReverseNumberDigits() {
+        assertEquals("Testing with 12345", "5\n4\n3\n2\n1\n", getReversedDigitsOutput(12345));
+        assertEquals("Testing with 987654321", "1\n2\n3\n4\n5\n6\n7\n8\n9\n", getReversedDigitsOutput(987654321));
+        assertEquals("Testing with 0", "0\n", getReversedDigitsOutput(0));
+        assertEquals("Testing with a negative number -123", "3\n2\n1\n", getReversedDigitsOutput(-123));
+    }
+    private String getReversedDigitsOutput(int n) {
+        java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(outContent));
+        ReverseNumberDigits.main(null);
+        return outContent.toString();
+    }
+}
