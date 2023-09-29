7e4508bf7c94b1d64c4ffaf66d44c141a8367f0f
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2023-09-29 23:58:40 +0530

    Update Solution.java

--- javaProblems/problem 68/Solution.java
+++ javaProblems/problem 68/Solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ReverseNumberDigits {
+public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
