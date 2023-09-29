cb496a19acc0bddd7edf7343ef88d9955a817e7e
Naga Chaitanya Varma <69190057+Chay2203@users.noreply.github.com>
2023-09-30 00:10:18 +0530

    Update Solution.java

--- javaProblems/problem 68/Solution.java
+++ javaProblems/problem 68/Solution.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-        
-        while (n > 0) {
-            int digit = n % 10; 
-            System.out.println(digit); 
-            n = n / 10; 
+    public int solve(int N) {
+        int reversed = 0;
+        while (N != 0) {
+            int digit = N % 10;
+            reversed = reversed * 10 + digit;
+            N /= 10;
         }
-        
-        sc.close();
+        return reversed;
     }
 }
